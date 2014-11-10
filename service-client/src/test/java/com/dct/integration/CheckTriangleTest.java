2dc3902c12412da9119fbd12d4086b1734d9a128
Kozlova Elena <elena.kozlova@oxagile.com>
2014-11-10 17:49:37 +0300

    Refactor integration tests

--- service-client/src/test/java/com/dct/integration/CheckTriangleTest.java
+++ service-client/src/test/java/com/dct/integration/CheckTriangleTest.java
@@ -2,87 +2,81 @@ package com.dct.integration;
 
 import com.dct.IntegrationTests;
 import com.dct.TestUtils;
-import com.dct.core.data.Message;
+import com.dct.client.TriangleClient;
 import com.dct.service.request.TriangleRequest;
+import com.dct.service.response.BaseErrorResponse;
 import com.dct.service.response.TriangleExistsEnum;
 import com.dct.service.response.TriangleResponse;
 import com.dct.service.response.VersionResponse;
 import com.dct.exception.HttpStatusException;
-import com.dct.client.RestHttpClient;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.GenericType;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 
 import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 
+@ContextConfiguration(locations = {"classpath:/web-context.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
 @Category(IntegrationTests.class)
 public class CheckTriangleTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CheckTriangleTest.class);
 
-    private RestHttpClient restHttpClient;
+    @InjectMocks
+    @Autowired
+    private TriangleClient triangleClient;
 
     @Before
     public void setUp() {
-        Client client = Client.create();
-        restHttpClient = new RestHttpClient(client);
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void testCheckTriangleRequestYes() throws IOException, HttpStatusException {
         TriangleRequest triangleRequest = TestUtils.getCheckTriangleRequestYes();
+        TriangleResponse triangleResponse = triangleClient.checkTriangle(triangleRequest);
 
-        Message<TriangleResponse> triangleResponse = (Message<TriangleResponse>) restHttpClient.post(MediaType.APPLICATION_JSON_TYPE,
-                triangleRequest, TestUtils.getCheckTriangleTestApiUrl(), new GenericType<Message<TriangleResponse>>(){});
         printTriangleResponse(triangleResponse);
-
-        assertThat("Triangle request occurred with result: YES", triangleResponse.getData().getExists(), is(equalTo(TriangleExistsEnum.YES)));
+        assertThat("Triangle request occurred with result: YES", triangleResponse.getExists(), is(equalTo(TriangleExistsEnum.YES)));
     }
 
     @Test
     public void testCheckTriangleRequestNo() throws IOException, HttpStatusException {
         TriangleRequest triangleRequest = TestUtils.getCheckTriangleRequestNo();
+        TriangleResponse triangleResponse = triangleClient.checkTriangle(triangleRequest);
 
-        Message<TriangleResponse> triangleMessageResponse = (Message<TriangleResponse>) restHttpClient.post(MediaType.APPLICATION_JSON_TYPE,
-                triangleRequest, TestUtils.getCheckTriangleTestApiUrl(), new GenericType<Message<TriangleResponse>>(){});
-        printTriangleResponse(triangleMessageResponse);
-
-        assertThat("Triangle request occurred with result: NO", triangleMessageResponse.getData().getExists(), is(equalTo(TriangleExistsEnum.NO)));
+        printTriangleResponse(triangleResponse);
+        assertThat("Triangle request occurred with result: NO", triangleResponse.getExists(), is(equalTo(TriangleExistsEnum.NO)));
     }
 
-    @Test
+    @Test(expected = HttpStatusException.class)
     public void testCheckTriangleRequestError() throws IOException, HttpStatusException {
         TriangleRequest triangleRequest = TestUtils.getCheckTriangleRequestError();
-
-        Message<TriangleResponse> triangleResponse = (Message<TriangleResponse>) restHttpClient.post(MediaType.APPLICATION_JSON_TYPE,
-                triangleRequest, TestUtils.getCheckTriangleTestApiUrl(), new GenericType<Message<TriangleResponse>>(){});
-        printTriangleResponse(triangleResponse);
-
-        assertThat("Triangle request occurred with result: Error", triangleResponse.getErrorCode(), not(equalTo(0)));
+        triangleClient.checkTriangle(triangleRequest);
     }
 
     @Test
     public void testGetApplicationVersion() throws IOException, HttpStatusException {
-        Message<VersionResponse> versionResponse = (Message<VersionResponse>) restHttpClient.get(MediaType.APPLICATION_JSON_TYPE,
-                TestUtils.getVersionTestApiUrl(), new GenericType<Message<VersionResponse>>(){});
+        VersionResponse versionResponse = triangleClient.getServiceVersion();
         printTriangleResponse(versionResponse);
-
-        assertThat("Get version request occurred with result: 1.0-SNAPSHOT", versionResponse.getData().getVersion(), is(equalTo("1.0-SNAPSHOT")));
+        assertThat("Get version request occurred with result: 1.0-SNAPSHOT", versionResponse.getVersion(), is(equalTo("1.0-SNAPSHOT")));
     }
 
-    protected void printTriangleResponse(Message response) {
-        LOGGER.debug("Message response: {}", reflectionToString(response, ToStringStyle.SHORT_PREFIX_STYLE));
+    protected void printTriangleResponse(BaseErrorResponse baseErrorResponse) {
+        LOGGER.debug("Triangle response: {}", reflectionToString(baseErrorResponse, ToStringStyle.SHORT_PREFIX_STYLE));
     }
 }
