6fb039f075fcfc06b134f4f8e7aae69b357d0ea0
Kozlova Elena <elena.kozlova@oxagile.com>
2014-11-13 15:12:42 +0300

    Refactor RestClient + add javadoc

--- service/src/main/java/com/dct/client/RestHttpClient.java
+++ service/src/main/java/com/dct/client/RestHttpClient.java
@@ -3,7 +3,6 @@ package com.dct.client;
 import com.dct.exception.HttpStatusException;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +10,15 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+/**
+ * RestHttpClient - allows to perform REST calls.
+ *
+ * @param <P> Object type to be Posted.
+ * @param <R> Object type expected as Return.
+ *           <p/>
+ *           Methods in this class return the expected instance or @see HttpStatusException.
+ *           The exception allows to get HTTP response status and deserizlize response.
+ */
 public class RestHttpClient<P, R> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RestHttpClient.class);
@@ -21,30 +29,31 @@ public class RestHttpClient<P, R> {
         this.client = client;
     }
 
+    /**
+     * POST method
+     *
+     * @throws HttpStatusException
+     */
     public R post(MediaType mediaType, P entityToPost, String targetURI, Class<R> entityResponseClass) throws HttpStatusException {
         WebResource.Builder builder = getBuilder(mediaType, targetURI);
         ClientResponse response = builder.post(ClientResponse.class, entityToPost);
         return getResponse(entityResponseClass, response);
     }
 
-    public R post(MediaType mediaType, P entityToPost, String targetURI, GenericType<R> entityResponseClass) throws HttpStatusException {
-        WebResource.Builder builder = getBuilder(mediaType, targetURI);
-        ClientResponse response = builder.post(ClientResponse.class, entityToPost);
-        return getResponse(entityResponseClass, response);
-    }
-
+    /**
+     * Get method
+     *
+     * @throws HttpStatusException
+     */
     public R get(MediaType mediaType, String targetURI, Class<R> entityResponseClass) throws HttpStatusException {
         WebResource.Builder builder = getBuilder(mediaType, targetURI);
         ClientResponse response = builder.get(ClientResponse.class);
         return getResponse(entityResponseClass, response);
     }
 
-    public R get(MediaType mediaType, String targetURI, GenericType<R> entityResponseClass) throws HttpStatusException {
-        WebResource.Builder builder = getBuilder(mediaType, targetURI);
-        ClientResponse response = builder.get(ClientResponse.class);
-        return getResponse(entityResponseClass, response);
-    }
-
+    /**
+     * Creates a WebResource.Builder according to given parameters.
+     */
     private WebResource.Builder getBuilder(MediaType mediaType, String targetURI) {
         WebResource webResource = client.resource(targetURI);
         WebResource.Builder builder = webResource.type(mediaType).accept(mediaType);
@@ -54,20 +63,13 @@ public class RestHttpClient<P, R> {
         return builder;
     }
 
+    /**
+     * Returns deserialized response or throws @see HttpStatusException if the response status is not 200.
+     */
     private R getResponse(Class<R> entityResponseClass, ClientResponse response) throws HttpStatusException {
         int status = response.getStatus();
         LOGGER.info("Status [{}] ", status);
-        if (status == Response.Status.OK.getStatusCode() || status == Response.Status.CREATED.getStatusCode()) {
-            return response.getEntity(entityResponseClass);
-        } else {
-            throw new HttpStatusException("Request unsuccessful", status, response);
-        }
-    }
-
-    private R getResponse(GenericType<R> entityResponseClass, ClientResponse response) throws HttpStatusException {
-        int status = response.getStatus();
-        LOGGER.info("Status [{}] ", status);
-        if (status == Response.Status.OK.getStatusCode() || status == Response.Status.CREATED.getStatusCode()) {
+        if (status == Response.Status.OK.getStatusCode()) {
             return response.getEntity(entityResponseClass);
         } else {
             throw new HttpStatusException("Request unsuccessful", status, response);
